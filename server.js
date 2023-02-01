5c44eeee6bd7c6d1aad89069ccff3723ce371241
omjiverma <omjidvlpr@gmail.com>
2023-02-01 14:03:50 +0530

    simple server setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log(" Hello World")
+const http = require("http");
+const app = require("./app");
+
+PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+function startServer() {
+  server.listen(PORT, () => {
+    console.log(`Server is listening at Port ${PORT} .....`);
+  });
+}
+
+startServer();
