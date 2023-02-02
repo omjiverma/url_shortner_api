cbe84841355b9a001274394c605a8662a98d8000
omjiverma <omjidvlpr@gmail.com>
2023-02-02 09:41:58 +0530

    Connecting with MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const http = require("http");
 const app = require("./app");
-require('dotenv').config()
+const mongoose = require("mongoose");
 
-PORT = process.env.PORT || 8000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+mongoose.set('strictQuery', false);
 
 const server = http.createServer(app);
 
-function startServer() {
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB ...");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
+
+async function startServer() {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   server.listen(PORT, () => {
     console.log(`Server is listening at Port ${PORT} .....`);
   });
